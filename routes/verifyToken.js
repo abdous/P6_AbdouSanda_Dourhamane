334c27698707e3fb25e8432e565ab0e6b6025bad
dourhamane <dourhamane.abdousanda@delijn.be>
2024-01-21 14:38:37 +0100

    add verification token for when user is loged .

---
+++ routes/verifyToken.js
@@ -0,0 +1,16 @@
+const JWT = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+    const token = req.header('auth-token')
+    if (!token) return res.status(401).send("Access-Denied")
+
+    try {
+        const verified = JWT.verify(token, process.env.TOKEN_SECRET)
+        req.user = verified
+
+        next()
+
+    } catch (err) {
+        res.status(404).send({ message: "Invalid User" })
+    }
+}
