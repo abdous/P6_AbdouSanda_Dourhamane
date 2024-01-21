490fd875b5445415a6da70f6902b751b837f706e
dourhamane <dourhamane.abdousanda@delijn.be>
2024-01-21 14:31:19 +0100

    add validations.

--- validation.js
+++ validation.js
@@ -1,20 +1,22 @@
 const Joi = require('@hapi/joi')
+
+
 const loginValidation = (data) => {
 
     const schema = Joi.object({
         email: Joi.string().min(6).required().email(),
-        passWord: Joi.string().min(6).required()
+        password: Joi.string().min(6).required()
     })
     return schema.validate(data)
 }
-const registrationValidation = (data) => {
+const signupValidation = (data) => {
 
     const schema = Joi.object({
         email: Joi.string().min(6).required().email(),
-        passWord: Joi.string().min(6).required()
+        password: Joi.string().min(6).required()
     })
     return schema.validate(data)
 }
 
 module.exports.loginValidation = loginValidation
-module.exports.registrationValidation = registrationValidation
+module.exports.signupValidation = signupValidation
