24ce1d4eda996b2a7eb2718d1d81a22786e0d423
dourhamane <dourhamane.abdousanda@delijn.be>
2024-01-24 10:01:57 +0100

    implement the update and deleted option for sauce in backend.

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,6 +1,7 @@
 
 const router = require("express").Router()
 const Sauce = require('../model/Sauce')
+const fs = require('fs')
 
 const multer = require('../routes/multer-config')
 
@@ -33,7 +34,7 @@ router.get('/:id', (req, res, next) => {
 })
 
 // post/add one sauce
-router.post('/', multer, (req, res) => { 
+router.post('/', multer, (req, res) => {
     const url = req.protocol + '://' + req.get('host')
     req.body.sauce = JSON.parse(req.body.sauce)
     console.log('url', req.file.filename)
@@ -60,6 +61,86 @@ router.post('/', multer, (req, res) => {
         })
     })
 })
+
+
+// update a sauce
+router.put('/:id', multer, (req, res, next) => {
+    let sauce = new Sauce({ _id: req.params._id })
+    if (req.file) {
+        const url = req.protocol + '://' + req.get('host')
+        req.body.sauce = JSON.parse(req.body.sauce)
+
+        sauce = {
+            _id: req.params.id,
+            userId: req.body.sauce.userId,
+            name: req.body.sauce.name,
+            manufacturer: req.body.sauce.manufacturer,
+            description: req.body.sauce.description,
+            mainPepper: req.body.sauce.mainPepper,
+            imageUrl: url + '/images/' + req.file.filename,
+            heat: req.body.sauce.heat,
+            likes: req.body.sauce.likes,
+            dislikes: req.body.sauce.dislikes,
+            usersLiked: req.body.sauce.usersLiked,
+            usersDisliked: req.body.sauce.usersDisliked,
+        }
+
+    } else {
+        sauce = {
+            userId: req.body.userId,
+            name: req.body.name,
+            manufacturer: req.body.manufacturer,
+            description: req.body.description,
+            mainPepper: req.body.mainPepper,
+            imageUrl: req.body.imageUrl,
+            heat: req.body.heat,
+            likes: req.body.likes,
+            dislikes: req.body.dislikes,
+            usersLiked: req.body.usersLiked,
+            usersDisliked: req.body.usersDisliked,
+        }
+    }
+    Sauce.updateOne({ _id: req.params.id }, sauce).then(
+        () => {
+            res.status(200).json({
+                message: 'Sauce Modified successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
+
+
+// delete a sauce
+router.delete('/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then(
+        (sauce) => {
+            const filenamne = sauce.imageUrl.split('/images/')[1]
+            fs.unlink('images/' + filenamne, () => {
+                Sauce.deleteOne({ _id: req.params.id }).then(
+                    () => {
+                        res.status(200).json({
+                            message: 'Sauce Successfully Deleted!'
+                        });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({
+                            error: error
+                        });
+                    }
+                );
+            })
+        }
+    )
+
+});
+
 
 module.exports = router
 
