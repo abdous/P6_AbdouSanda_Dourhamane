ba45fb5b5c6cc69d053ccd7a9e0b7927716ad4d3
dourhamane <dourhamane.abdousanda@delijn.be>
2024-02-04 15:18:18 +0100

    remove console.log.

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,13 +2,11 @@ const JWT = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
     const token = req.header('Authorization').split(' ')[1]
-    console.log("token", token)
     if (!token) return res.status(401).send("Access-Denied")
 
     try {
         const verified = JWT.verify(token, process.env.TOKEN_SECRET)
         req.user = verified
-        console.log(req.user)
         next()
 
     } catch (err) {
